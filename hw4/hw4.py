d9eba11f2f0a6dca92b773a46891cfde7a2a9fa1
victoriassazonova <31881591+victoriassazonova@users.noreply.github.com>
2017-11-20 14:01:46 +0300

    Delete hw4.py

--- hw4/hw4.py
+++
@@ -1,9 +0,0 @@
-f = open('text.txt', encoding='utf-8')
-k = 0
-for line in f:
-    s = line.split()
-    for i in range (len(s)):
-        s[i] = len(s[i])
-        if s[i] > 10:
-            k += 1 
-print(k)
