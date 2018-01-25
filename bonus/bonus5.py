94590ca04a8eafa70838d02d014d16eb210691ec
victoriassazonova <31881591+victoriassazonova@users.noreply.github.com>
2018-01-25 17:34:50 +0300

    Add files via upload

---
+++ bonus/bonus5.py
@@ -0,0 +1,11 @@
+aig = 'aig'
+vvod = input('Введите слово(латиницей):')
+vivod=str()
+while len(vvod)> 0:
+    bukva = vvod[0]
+    if bukva == ('a' or 'i' or 'u' or 'o' or 'e' or 'y'):
+        vivod = vivod+bukva
+    else:
+        vivod = vivod + bukva + aig 
+    vvod=vvod[1:]
+print (vivod)
