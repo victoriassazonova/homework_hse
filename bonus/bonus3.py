a37ad2f4fffc0270951b8b122b905f78af6a6624
victoriassazonova <31881591+victoriassazonova@users.noreply.github.com>
2018-01-26 14:22:23 +0300

    Update bonus3.py

--- bonus/bonus3.py
+++ bonus/bonus3.py
@@ -3,12 +3,12 @@ vvod = input('Введите слово(латиницей):')
 if len(vvod)> 0:
     slovo = vvod.lower()
     bukva = slovo[0]
-    if bukva == ('a' or 'i' or 'u' or 'o' or 'e'or 'y'):
+    if bukva == 'a' or 'i' or 'u' or 'o' or 'e'or 'y':
         vivod = slovo + pyg
         print (vivod)
     else:
         bukva_new = slovo[1]
-        if bukva_new == ('y' or 'a' or 'u' or 'i' or 'o' or 'e'):
+        if bukva_new == 'y' or 'a' or 'u' or 'i' or 'o' or 'e':
             print (slovo[1:] + bukva + pyg)
         else:
             print (slovo[2:] + bukva + bukva_new + pyg)
