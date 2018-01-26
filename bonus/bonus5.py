620e872d09b165cb45a4a4138ea93eb50a3c1670
victoriassazonova <31881591+victoriassazonova@users.noreply.github.com>
2018-01-26 14:22:57 +0300

    Update bonus5.py

--- bonus/bonus5.py
+++ bonus/bonus5.py
@@ -3,7 +3,7 @@ vvod = input('Введите слово(латиницей):')
 vivod=str()
 while len(vvod)> 0:
     bukva = vvod[0]
-    if bukva == ('a' or 'i' or 'u' or 'o' or 'e' or 'y'):
+    if bukva == 'a' or 'i' or 'u' or 'o' or 'e' or 'y':
         vivod = vivod+bukva
     else:
         vivod = vivod + bukva + aig 
