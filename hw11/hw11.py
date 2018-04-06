774bb3003b9af72edd5537d58b6955de43b67884
victoriassazonova <31881591+victoriassazonova@users.noreply.github.com>
2018-04-06 14:15:02 +0300

    Add files via upload

---
+++ hw11/hw11.py
@@ -0,0 +1,20 @@
+import re
+
+
+def search():
+    with open('dino.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            result = re.sub('динозавр', 'кот', line)
+            sresult = re.sub('Динозавр', 'Кот', result)
+            tresult = re.sub('Диноза́вры', 'Коты́', sresult)
+            with open('cat.txt', 'a', encoding='utf-8') as t:
+                t.write(tresult) 
+            
+search()
+
+#я не стала делать проверку на прилагательные от этого слова и тд,
+#потому что от слова динозавр никаких форм кроме его падежей и чисел не образуется
+#(во всяком случае насколько мне известно)
+        
+
+
